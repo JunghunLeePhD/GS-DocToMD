79e4a3a595d30ee3f807f6caafb4d1ccc80b50f4
JunghunLeePhD <yijeonghunxd@gmail.com>
2025-11-22 05:20:31 +0000

    Update (`Code.js`): run 'Index.html' on webserver

--- src/Code.js
+++ src/Code.js
@@ -1,4 +1,8 @@
-function sayHello() {
-  const message = "Hello, World! The time is " + new Date();
-  Logger.log(message);
+/**
+ * Serves the HTML page when the web app is visited.
+ */
+function doGet() {
+  return HtmlService.createHtmlOutputFromFile("Index")
+    .setTitle("Doc To Markdown")
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
